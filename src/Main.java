bd95c29c95a360cbac4153faa4e47deaa73813e7
NikolayKir <107439486+NikolayKir@users.noreply.github.com>
2022-06-26 23:48:34 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int NachShchet = 50;
+        int nachshchet = 50;
         int sum = 1100;
         int bonus = 0;
-        int ItogSchet = 0;
+        int itogschet = 0;
 
         if (sum >= 1000) {
             bonus = sum / 100;
         }
-        ItogSchet = NachShchet + bonus + sum;
-        System.out.println("Итоговый счет: " + ItogSchet + ". Ваши бонусные рубли: " + bonus);
+        itogschet = nachshchet + bonus + sum;
+        System.out.println("Итоговый счет: " + itogschet + ". Ваши бонусные рубли: " + bonus);
     }
 }
